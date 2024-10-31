ae94b41b70c97c38e7abdefa23ba6cbb5df49891
panther865 <77604848+panther865@users.noreply.github.com>
2024-11-01 01:42:32 +0530

    portfolio

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -8,10 +8,10 @@ function Projects() {
     <ProjectCard title="Gossip chat" imgSrc="/pictures/gossip_chat.png" link="https://gossipchats.netlify.app/" skills={['react', 'firebase', 'node']} />
     <ProjectCard title="Yummy " imgSrc="/pictures/yummy.png" link="https://yummyfortummy.netlify.app/" skills={['react','node','express']} />
     <ProjectCard title="Medi-buddy " imgSrc="/pictures/medibuddy.png" link="https://github.com/Adi-3000/medibuddy" skills={['HTML', 'CSS', 'JS','sql','php']} />
-    <ProjectCard  title="product page" imgSrc="/pictures/product.png" link="" skills={['HTML', 'CSS', 'JS']} />
-    <ProjectCard  title="Tribute page" imgSrc="/pictures/tribute.png" link="" skills={['HTML', 'CSS', 'JS']} />
-    <ProjectCard title="calculator " imgSrc="/pictures/calci.png" link="" skills={['HTML', 'CSS', 'JS']} />
-    <ProjectCard  title="quizie" imgSrc="/pictures/quiz.png" link="" skills={['HTML', 'CSS', 'JS']} />
+    <ProjectCard  title="product page" imgSrc="/pictures/product.png" link="https://adi-3000.github.io/demo-product-landing-page/" skills={['HTML', 'CSS', 'JS']} />
+    <ProjectCard  title="Tribute page" imgSrc="/pictures/tribute.png" link="https://adi-3000.github.io/responsive-tribute-page/" skills={['HTML', 'CSS']} />
+    <ProjectCard title="calculator " imgSrc="/pictures/calci.png" link="https://adi-3000.github.io/js-calculator/" skills={['HTML', 'CSS', 'JS']} />
+    <ProjectCard  title="quizie" imgSrc="/pictures/quiz.png" link="https://adi-3000.github.io/js_quiz/" skills={['HTML', 'CSS', 'JS']} />
   </div>
   )
 }
